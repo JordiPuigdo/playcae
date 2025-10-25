a98b56439ab77c3ad96ee4afc33ab89f6e8950bd
JordiPuigdo <jordipuigdollers1992@gmail.com>
2025-10-25 17:01:08 +0200

    add tag manager

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -1,12 +1,14 @@
 import { ReactNode } from "react";
 import Header from "./components/landing/Header";
 import Footer from "./components/landing/Footer";
+import TagManager from "./components/TagManage";
 
 // app/(web)/layout.tsx - Solo para sub-rutas de web
 export default function WebLayout({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-screen bg-background">
       <Header />
+      <TagManager />
       {children}
       <Footer />
     </div>
